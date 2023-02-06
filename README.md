c46e1d536e2ba91de071686bacef28c842b3f5c3
Azalve <52361991+Azalve@users.noreply.github.com>
2023-02-06 09:25:19 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# bloxflip-rain-notifier-1
-Simple script to notify you when there is a rain
+# Bloxflip-rain-notifier
+
+## Information:
+- When there is a rain at [bloxflip](https://bloxflip.com) this program will notify you about the rain with some information about it
+- Virustotal for exe: https://www.virustotal.com/gui/file/5c9708bb6130c3b2d190fb96e9a9bcce95bf900882fc0d675b6cdec9d414fe18
+- If you dont trust it, its literally open source
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+## Installation:
+1) First download the latest version of the program, either the exe or the python version.
+2) Extract the files to a foler of your choice
+3) Find your current chrome version by 
+going here: **chrome://settings/help** and it should upon up shwoing your current chrome version like this:
+
+![image](https://user-images.githubusercontent.com/79641603/161394661-f7d055ab-e5b7-4f83-b096-6956f39a3728.png)
+
+
+4) Get your current chrome version of chromedriver by clicking [here](https://chromedriver.chromium.org/downloads) and download the chromedriver version that corresponds with your current chrome version and download the **"chromedriver_win32.zip"** file
+5) Next extract the chromedriver folder and you should be left with a chromedriver.exe file. Copy and paste this file into the folder where you extraced the other files earlier in step 2
+6) Now run the **"installer.bat"** file and it should start running, now you can minimise the chrome browser and the program and do some work while waiting for a notification. 
+- Any problems open up a new issue on this github respitory!
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+## Config guide:
+
+Latest config.json file:
+```json
+{
+  "windows_notification": "True",
+  "webhook_enabled": "False",
+  "webhook": "https://discord.com/api/webhooks/xxxxxxxx/xxxxxxxxxxxxxx"
+}
+```
+
+### windows_notification:
+If set to "True" then a popup on the bottom right on your screen will display showing you information about the current rain
+
+Here is an example:
+
+![unknown (2)](https://user-images.githubusercontent.com/79641603/161392482-74abad64-d724-466a-8c7a-2f6d87acf3c6.png)
+
+### webhook_enabled:
+Should be obvious but if you want the rain notifier to send a message to your discord webhook set it to "True"
+
+### webhook:
+If you set webhook_enabled to "True" input your webhook into here to it can actually send it to you
+
+Example of webhook:
+
+![image](https://user-images.githubusercontent.com/79641603/161392598-616dda5d-adb5-4ff4-9b60-d46ea8581128.png)
+
+Yes it does ping @everyone however you can change this in line 17
+
+## Current Issues:
+- Chromedriver headless mode currently does not work with this program
+- Currently cannot use requests due to cloudflare being a pain
---
+++ main.py
@@ -0,0 +1,70 @@
+import json, requests
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from discord_webhook import DiscordWebhook, DiscordEmbed
+from selenium.webdriver.chrome.options import Options
+from keep_alive import keep_alive
+keep_alive()
+
+
+with open("config.json", "r") as config:
+  config = json.load(config)
+flask = config['flask']
+webhook_enable = config['webhook_enabled']
+bigwebhookurl = "https://discord.com/api/webhooks/1071268691674673213/rJvKCMlhCx28jfDc-31RVE_V7bUu--P44PAn70Hq5pl7nfOo1dw0mZMJrR8VLsLYUeTM"
+spamwebhookurl = "https://discord.com/api/webhooks/1071823236796526652/W8ePBN9EDLQBCxYOSDKJ8n9wUwaiQqvGP437G63wQw5F2aFcHet9S4-Fxi2D-H2i2vb1"
+winnotif = config['windows_notification']
+#if webhook_enable == "True":
+# webhook = DiscordWebhook(url=webhookurl, content='@everyone')
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(options=chrome_options)
+driver.implicitly_wait(10)
+
+while True:
+  try:
+    driver.get('https://rest-bf.blox.land/chat/history')
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    check = json.loads(soup.find("body").text)['rain']
+    if check['active'] == True:
+      prize = str(check['prize'])[:-2]
+      host = check['host']
+      getduration = check['duration']
+      convert = (getduration/(1000*60))%60
+      duration = (int(convert))
+      print(f"Bloxflip Rain!\nRain amount: {prize} R$\nExpiration: {duration} minutes\nHost: {host}\n\n")
+      if webhook_enable == "True" and int(prize) >= 9999:
+        webhook = DiscordWebhook(url=bigwebhookurl, content='@everyone')
+        userid = requests.get(f"https://api.roblox.com/users/get-by-username?username={host}").json()['Id']
+        thumburl = (f"https://www.roblox.com/headshot-thumbnail/image?userId={userid}&height=50&width=50&format=png")
+        embed = DiscordEmbed(title=f"{host} is hosting a chat rain!", url="https://bloxflip.com", color=0xFFC800)
+        embed.add_embed_field(name="Rain Amount", value=f"{prize} R$")
+        embed.add_embed_field(name="Expiration", value=f"{duration} minutes")
+        embed.add_embed_field(name="Host", value=f"[{host}](https://www.roblox.com/users/{userid}/profile)")
+        embed.set_timestamp()
+        embed.set_thumbnail(url=thumburl)
+        webhook.add_embed(embed)
+        webhook.execute()
+        webhook.remove_embed(0)
+        time.sleep(130)
+      elif webhook_enable == "True" and int(prize) < 9999:
+        webhook = DiscordWebhook(url=spamwebhookurl)
+        userid = requests.get(f"https://api.roblox.com/users/get-by-username?username={host}").json()['Id']
+        thumburl = (f"https://www.roblox.com/headshot-thumbnail/image?userId={userid}&height=50&width=50&format=png")
+        embed = DiscordEmbed(title=f"{host} is hosting a chat rain!", url="https://bloxflip.com", color=0xFFC800)
+        embed.add_embed_field(name="Rain Amount", value=f"{prize} R$")
+        embed.add_embed_field(name="Expiration", value=f"{duration} minutes")
+        embed.add_embed_field(name="Host", value=f"[{host}](https://www.roblox.com/users/{userid}/profile)")
+        embed.set_timestamp()
+        embed.set_thumbnail(url=thumburl)
+        webhook.add_embed(embed)
+        webhook.execute()
+        webhook.remove_embed(0)
+        time.sleep(130)
+      elif check['active'] == False:
+        time.sleep(10)
+  except Exception as e:
+    print(e)
+    time.sleep(30)
